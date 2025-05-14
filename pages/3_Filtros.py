6d69856de6c935930c5aa0b9efddab825f86058f
FulPrawm <leonardo.p03@hotmail.com>
2025-05-14 15:23:55 -0300

    Update 3_Filtros.py

--- pages/3_Filtros.py
+++ pages/3_Filtros.py
@@ -3,9 +3,38 @@ import plotly.express as px
 import pandas as pd
 
 # Carregar dados
-sessao = pd.read_excel("sessao.xlsx")
-df = pd.read_excel("performance.xlsx")
-df1 = pd.read_excel("vitals.xlsx")
+df = pd.read_excel('performance.xlsx', header=4, skiprows=[5])
+df.columns = df.columns.str.strip()
+df = df.iloc[:, 6:]
+first_col = df.columns[0]
+df[first_col] = (
+    df[first_col]
+    .astype(str)
+    .str.strip()
+    .str.replace(',', '.', regex=False)
+)
+df[first_col] = pd.to_numeric(df[first_col], errors='coerce')
+df[first_col] = df[first_col] / 1000
+for col in df.columns[1:]:
+    df[col] = (df[col])
+
+df1 = pd.read_excel("vitals.xlsx", header=4, skiprows=[5])
+df1.columns = df1.columns.str.strip()
+df1 = df1.iloc[:, 6:]
+first_col = df1.columns[0]
+df1[first_col] = (
+    df1[first_col]
+    .astype(str)
+    .str.strip()
+    .str.replace(',', '.', regex=False)
+)
+df1[first_col] = pd.to_numeric(df1[first_col], errors='coerce')
+df1[first_col] = df1[first_col] / 1000
+for col in df1.columns[1:]:
+    df1[col] = (df1[col])
+
+sessao = pd.read_excel('sessao.xlsx')
+pd.set_option('display.max_rows', None)
 
 def filtrar_e_exibir_kpi(df, df1):
     st.title("Filtragem - KPI e Vitals")
