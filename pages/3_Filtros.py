866418f3b101f25dc1a75c5d97f630bad1658b55
FulPrawm <leonardo.p03@hotmail.com>
2025-05-14 14:51:10 -0300

    Update 3_Filtros.py

--- pages/3_Filtros.py
+++ pages/3_Filtros.py
@@ -8,13 +8,11 @@ def filtrar_e_exibir_kpi(df, df1):
     valor_maximo = st.number_input("Valor máximo (KPI/Vitals)", min_value=0, max_value=200, value=150)
 
     # Filtro e gráfico - KPI
-    if "Calc Lap Time [s]" in df.columns:
         df_filter = df[(df["Calc Lap Time [s]"] >= valor_minimo) & (df["Calc Lap Time [s]"] <= valor_maximo)]
         fig1 = px.box(df_filter, x="Calc Lap Time [s]")
         st.plotly_chart(fig1, key="kpi_graph")
 
     # Filtro e gráfico - Vitals
-    if "Calc Lap Time [s]" in df1.columns:
         df1_filter = df1[(df1["Calc Lap Time [s]"] >= valor_minimo) & (df1["Calc Lap Time [s]"] <= valor_maximo)]
         fig2 = px.box(df1_filter, x="Calc Lap Time [s]")
         st.plotly_chart(fig2, key="vitals_graph")
@@ -22,10 +20,7 @@ def filtrar_e_exibir_kpi(df, df1):
 
 def filtrar_e_exibir_sessao(sessao):
     st.title("Filtragem - Race Session")
-
     valor_maximo = st.number_input("Valor máximo (Race Time)", min_value=0, max_value=200, value=110)
-    
-    if "Lap Tm (S)" in sessao.columns:
         sessao_filtrado = sessao[sessao["Lap Tm (S)"] <= valor_maximo]
         fig = px.box(sessao_filtrado, x="Lap Tm (S)")
         st.plotly_chart(fig)
