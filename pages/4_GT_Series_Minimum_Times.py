6774bdbc25f42e81ae5f49f49425ddc8f8f8440a
FulPrawm <leonardo.p03@hotmail.com>
2025-07-11 16:10:24 -0300

    Update 4_GT_Series_Minimum_Times.py

--- pages/4_GT_Series_Minimum_Times.py
+++ pages/4_GT_Series_Minimum_Times.py
@@ -18,7 +18,7 @@ st.image('header.png')
 st.title("Fastest Time Session Data Report")
 
 # Caminho base onde ficam as pastas das etapas
-PASTA_ETAPAS = "Arquivos Treinos & Qualy"
+PASTA_ETAPAS = "Arquivos GT Minimum"
 
 # Lista todas as etapas (pastas dentro de "resultados")
 etapas_disponiveis = [p for p in os.listdir(PASTA_ETAPAS) if os.path.isdir(os.path.join(PASTA_ETAPAS, p))]
