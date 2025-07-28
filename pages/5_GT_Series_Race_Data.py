34840372e2b16a718ef1ea2b26b2eeb462be3124
FulPrawm <leonardo.p03@hotmail.com>
2025-07-28 10:38:39 -0300

    Update 5_GT_Series_Race_Data.py

--- pages/5_GT_Series_Race_Data.py
+++ pages/5_GT_Series_Race_Data.py
@@ -22,14 +22,6 @@ PASTA_ETAPAS = "Arquivos GT Race"
 
 # Lista todas as etapas (pastas dentro de "resultados")
 etapas_disponiveis = [p for p in os.listdir(PASTA_ETAPAS) if os.path.isdir(os.path.join(PASTA_ETAPAS, p))]
-
-st.header("Seletor de Etapa e Sessão")
-
-# Seletor de etapa
-etapa_escolhida = st.selectbox("Escolha a etapa:", sorted(etapas_disponiveis))
-
-# Lista arquivos de corrida (.xlsx) dentro da etapa selecionada
-pasta_etapa = os.path.join(PASTA_ETAPAS, etapa_escolhida)
 etapas_opcoes = ["Selecione uma etapa..."] + sorted(etapas_disponiveis)
 etapa_escolhida = st.selectbox("Escolha a etapa:", etapas_opcoes)
 
