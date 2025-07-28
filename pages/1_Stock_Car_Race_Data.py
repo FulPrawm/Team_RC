928cbbd53f4f7432dc19c38281f13d6f3fd8cade
FulPrawm <leonardo.p03@hotmail.com>
2025-07-28 14:32:26 -0300

    Update 1_Stock_Car_Race_Data.py

--- pages/1_Stock_Car_Race_Data.py
+++ pages/1_Stock_Car_Race_Data.py
@@ -247,22 +247,22 @@ elif option == 'BoxPlots':
         )
         st.plotly_chart(fig)
         
-            # Bloco 2 — por Car_ID (como rótulo) em tabs
-            tabs_box = st.tabs(["Volta", "S1", "S2", "S3", "SPT"])
-            colunas_boxplot = {
-                "Volta": "Lap Tm (S)",
-                "S1": "S1 Tm",
-                "S2": "S2 Tm",
-                "S3": "S3 Tm",
-                "SPT": "SPT"
-            }
+        # Bloco 2 — por Car_ID (como rótulo) em tabs
+        tabs_box = st.tabs(["Volta", "S1", "S2", "S3", "SPT"])
+        colunas_boxplot = {
+          "Volta": "Lap Tm (S)",
+          "S1": "S1 Tm",
+          "S2": "S2 Tm",
+          "S3": "S3 Tm",
+          "SPT": "SPT"
+         }
         
-            cores_carros = {
-                "Carro 10": "red",
-                "Carro 11": "blue",
-                "Carro 44": "gray",
-                "Carro 88": "yellow"
-            }
+         cores_carros = {
+          "Carro 10": "red",
+          "Carro 11": "blue",
+          "Carro 44": "gray",
+          "Carro 88": "yellow"
+         }
         
             for i, (tab_nome, coluna) in enumerate(colunas_boxplot.items()):
                 with tabs_box[i]:
