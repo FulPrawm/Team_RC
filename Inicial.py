1b83c913e887b8ba5528046f99a5ca3b137e5be4
FulPrawm <leonardo.p03@hotmail.com>
2025-08-13 09:49:24 -0300

    Update and rename Instructions.py to Inicial.py

--- Inicial.py
+++ Inicial.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 #header
 st.image('header.png')
-st.title('Data Report')
+st.title('Data Report - Instructions')
 st.header('Dados referentes as etapas de 2025')
 st.write('Por favor selecione a opção desejada no menu ao lado')
 st.write('Todos os dados são filtrados com 4% do menor tempo de volta e mínimo de 50% das voltas do lider')
@@ -12,3 +12,4 @@ st.write('Practice - gráficos referentes a cronometragem dos treinos/qualy por 
 st.write('Também é possível selecionar a etapa e a sessão desejada')
 st.write("Páginas GT Series é APENAS para GT Series")
 st.write("Caso queiram que eu faça um da Nascar, preciso do arquivo CSV deles")
+
