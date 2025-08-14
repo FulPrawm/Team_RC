31b018e3dde0177a4088a3744d597bb1b399013e
FulPrawm <leonardo.p03@hotmail.com>
2025-08-14 16:13:26 -0300

    Update 2_Stock_Car_Race_Data.py

--- pages/2_Stock_Car_Race_Data.py
+++ pages/2_Stock_Car_Race_Data.py
@@ -243,7 +243,7 @@ if etapa_escolhida != "Select a round...":
                 .apply(highlight_montadora, subset=["Manufacturer"])
             )
             st.subheader("Table ordered by Manufacturer")
-            st.dataframe(tabela3, hide_index=True, column_config={"B": None})
+            st.dataframe(tabela3, hide_index=True, column_config={"": None})
 
         
         elif option == 'Lines':
@@ -519,6 +519,7 @@ if etapa_escolhida != "Select a round...":
         st.warning("Please, select a race.")
 else:
     st.warning("Please, select a round.")
+
 
 
 
