e4f7e5eaff1f3fc80b0a97c8b600ebe6f9633a45
FulPrawm <leonardo.p03@hotmail.com>
2025-09-06 06:51:45 -0300

    Update 6_Watch.py

--- pages/6_Watch.py
+++ pages/6_Watch.py
@@ -18,9 +18,7 @@ tz = ZoneInfo("America/Sao_Paulo")
 # Session schedule
 # ------------------------
 sessions = [
-    {"name": "TL2", "start": "2025-09-05 17:30", "duration": 30},
-    {"name": "Briefing CBA", "start": "2025-09-05 18:00", "duration": 30},
-    {"name": "Shakedown - Grupo Único", "start": "2025-09-06 07:30", "duration": 20},
+    {"name": "Warm up - Grupo Único", "start": "2025-09-06 07:30", "duration": 30},
     {"name": "Retirada de Pneus Parque Fechado - Classificatório", "start": "2025-09-06 09:10", "duration": 20},
     {"name": "Qualy", "start": "2025-09-06 10:10", "duration": 50},
     {"name": "Retirada de Pneus Parque Fechado - 1a. Corrida", "start": "2025-09-06 12:40", "duration": 20},
