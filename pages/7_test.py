04f36fef238f7d42b4ce583035f7e1b94bf50e61
FulPrawm <leonardo.p03@hotmail.com>
2025-09-17 09:05:10 -0300

    Update 7_test.py

--- pages/7_test.py
+++ pages/7_test.py
@@ -32,7 +32,7 @@ else:
             errors="coerce"
         )
         best_lap = df["Calc Lap Time [s]"].min()
-        cutoff = best_lap * 1.10
+        cutoff = best_lap * 1.07
         df = df[df["Calc Lap Time [s]"] <= cutoff]
 
     # escolha da coluna Y
