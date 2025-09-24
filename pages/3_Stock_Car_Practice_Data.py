dc92fa7320541392fd9e50efc25a135d5d2130d5
FulPrawm <leonardo.p03@hotmail.com>
2025-09-24 11:05:39 -0300

    Update 3_Stock_Car_Practice_Data.py

--- pages/3_Stock_Car_Practice_Data.py
+++ pages/3_Stock_Car_Practice_Data.py
@@ -481,6 +481,7 @@ if etapa_escolhida != "Select a round...":
                         tickmode='array',
                         tickvals=[0, 0.5, 1],
                         ticktext=['Slow', 'Average', 'Fast']
+                        tickfont=dict(color="black")  # <<< deixa o texto preto
                     )
                 )
             )
@@ -492,6 +493,7 @@ if etapa_escolhida != "Select a round...":
         st.warning("Please, select a session.")
 else:
     st.warning("Please, select a round.")
+
 
 
 
