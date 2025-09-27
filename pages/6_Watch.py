78fe6a253fdf9f6502053c671b4ac05353b0a0b3
FulPrawm <leonardo.p03@hotmail.com>
2025-09-27 09:19:29 -0300

    Update 6_Watch.py

--- pages/6_Watch.py
+++ pages/6_Watch.py
@@ -27,10 +27,9 @@ sessions = [
     {"name": "TL2 - 1° Grupo", "start": "2025-09-26 15:45", "duration": 30},
     {"name": "TL2 - 2° Grupo", "start": "2025-09-26 16:20", "duration": 30},
     {"name": "Briefing CBA", "start": "2025-09-26 18:00", "duration": 30},
-
-
-
-    
+    {"name": "Qualy", "start": "2025-09-27 09:55", "duration": 50},
+    {"name": "Box Aberto", "start": "2025-09-27 13:40", "duration": 13},
+    {"name": "Corrida 1", "start": "2025-09-27 14:30", "duration": 32}
 ]
 
 # Convert start times into timezone-aware datetime objects
