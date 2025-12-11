a1690c00b229f8774ca7b06e25af8c815e2a5303
FulPrawm <leonardo.p03@hotmail.com>
2025-12-11 18:18:06 -0300

    Update 3_Stock_Car_Practice_Data.py

--- pages/3_Stock_Car_Practice_Data.py
+++ pages/3_Stock_Car_Practice_Data.py
@@ -54,7 +54,7 @@ if etapa_escolhida != "Select a round...":
 
         # Creating 3 manufacturer groups
         carros_toyota = [301, 4, 30, 111, 38, 81, 5, 7, 9, 21, 72]
-        carros_mitsubishi = [101, 444, 44, 33, 29, 11, 121, 18, 10, 88]
+        carros_mitsubishi = [101, 444, 44, 33, 29, 11, 121, 18, 10, 88, 31]
         # Function that verifies if the car is Toyota, if not its Mitsubishi, and if its not either, its Chevrolet
         def brand(x):
             if x in carros_toyota:
@@ -85,7 +85,8 @@ if etapa_escolhida != "Select a round...":
                 121: 'Car Racing KTF', 101: 'Car Racing KTF',
                 7: 'FT Gazoo Racing', 9: 'FT Gazoo Racing',
                 0: 'Scuderia Chiarelli', 95: 'Scuderia Chiarelli',
-                6: 'A. Mattheis Motorsport', 72: 'FT Cavaleiro'
+                6: 'A. Mattheis Motorsport', 72: 'FT Cavaleiro',
+                31: 'RCM Motorsport'
             }
             return Teams_dict.get(x, None)
         # Creating a new column called "Team" and applying the function "Teams"
@@ -122,7 +123,8 @@ if etapa_escolhida != "Select a round...":
          121: 'Felipe Baptista', 101: 'Gianluca Petecof',
          7: 'JP Oliveira', 9: 'Arthur Gama',
          95: 'Lucas Kohl', 0: 'Cacá Bueno',
-         6: 'Hélio Castroneves', 72: 'Antonella Bassani'
+         6: 'Hélio Castroneves', 72: 'Antonella Bassani',
+         31: 'Marcos Regadas'
         }
         sessao['Driver'] = sessao['Car_ID'].map(drivers_dict)
 
@@ -523,6 +525,7 @@ if etapa_escolhida != "Select a round...":
         st.warning("Please, select a session.")
 else:
     st.warning("Please, select a round.")
+
 
 
 
