b2c54960fe5b5be4ce1f2cdded53db778db58fac
FulPrawm <leonardo.p03@hotmail.com>
2026-02-12 14:34:08 -0300

    mudei o o local das pastas dos arquivos excel

--- Data/Y25/Stock_Car_2025_Practice_Data.py
+++ Data/Y25/Stock_Car_2025_Practice_Data.py
@@ -24,7 +24,7 @@ def show():
 
 
     # Path to where the folders of the rounds are
-    PASTA_ETAPAS = "Arquivos_Treinos_&_Qualy"
+    PASTA_ETAPAS = "Data/Y25/Excel_Files/Practice_And_Qualy"
     # Lists every round (folders inside "resultados")
     etapas_disponiveis = [p for p in os.listdir(PASTA_ETAPAS) if os.path.isdir(os.path.join(PASTA_ETAPAS, p))]
     st.subheader("Round and Session Selector")
