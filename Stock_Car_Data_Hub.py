26d77d246428f7ca6972a260e5574572246048fa
FulPrawm <leonardo.p03@hotmail.com>
2026-03-06 12:42:22 -0300

    mudança data hub

--- Stock_Car_Data_Hub.py
+++ Stock_Car_Data_Hub.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from Data.Y25.Stock_Car_2025_Race_Data import show as show_race_2025
 from Data.Y25.Stock_Car_2025_Practice_Data import show as show_practice_2025
-# Future:
-# from Data.Y26.Stock_Car_2026_Race_Data import show as show_race_2026
-# from Data.Y26.Stock_Car_2026_Practice_Data import show as show_practice_2026
+from Data.Y26.Stock_Car_2026_Race_Data import show as show_race_2026
+from Data.Y26.Stock_Car_2026_Practice_Data import show as show_practice_2026
 
 st.title("Race Analysis System")
 
@@ -39,8 +38,7 @@ else:
 
     elif selected_year == "2026":
         if selected_session == "Race Data":
-            st.info("Race 2026 is not implemented yet.")
+            show_race_2026()
             # show_race_2026()
         elif selected_session == "Practice Data":
-            st.info("Practice 2026 is not implemented yet.")
-            # show_practice_2026()
+            show_practice_2026()
