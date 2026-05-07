84ba2d5c612735dd20c446be9e4d9be1f701725b
FulPrawm <leonardo.p03@hotmail.com>
2026-05-07 14:42:51 -0300

    adicionado opção selectbox

--- Stock_Car_Data_Hub.py
+++ Stock_Car_Data_Hub.py
@@ -18,7 +18,7 @@ with st.sidebar:
     year_options = ["Select a year...", "2025", "2026"]
     selected_year = st.selectbox("Choose the year:", year_options)
 
-    session_options = ["Select a session...", "Race Data", "Practice Data"]
+    session_options = ["Select a session...", "Race Data", "Practice Data", "Teste"]
     selected_session = st.selectbox("Choose the session:", session_options)
 
 # ======================================
